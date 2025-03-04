97ff5e535f64f3fb876f3807e862caaef3fc9404
Sameer Arora <sameerarora2807@gmail.com>
2025-03-04 10:38:38 +0530

    added day 03 solution 1 and 2 both

--- day03.java
+++ day03.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class day03 {
+    //question 2:https://leetcode.com/problems/number-of-equivalent-domino-pairs/description/?envType=problem-list-v2&envId=2030iluv
     public int numEquivDominoPairs(int[][] dominoes) {
         for(int[] i:dominoes) Arrays.sort(i);
         Arrays.sort(dominoes,(a,b)->Arrays.compare(a,b));
@@ -16,6 +17,33 @@ public class day03 {
             }
         }
         return cnt;
+    }
+    //question 1:https://leetcode.com/problems/flipping-an-image/?envType=problem-list-v2&envId=array
+    void flip(int[] arr)
+    {
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+
+        }
+    }
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int [] i:image)
+        {
+            flip(i);
+            for(int x=0;x<i.length;x++)
+            {
+                i[x]=i[x]^1;
+            }
+        }
+        return image;
 
     }
 }
